c7173b763b5a84269247e1f011ec4c3d0ba75149
damlatsr <damlatsr@gmail.com>
2020-10-08 14:13:50 +0000

    web.php-route edit

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/dna/&start={start}/&rows={rows}',[\App\Http\Controllers\ApiController::class, 'index'])->name('api-controller');
+Route::get('/dna/&start={start}&rows={rows}',[\App\Http\Controllers\ApiController::class, 'index'])->name('api-controller');
 
 Route::get('/', function () {
     return view('welcome');
